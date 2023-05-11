9158d5fa6e324970a876c72929431585c7178e15
abdulazizaldawood1 <133047582+abdulazizaldawood1@users.noreply.github.com>
2023-05-11 13:45:21 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="mt-5 p-4 bg-dark text-white text-center">
+  <p>BODY FIT GYM website IS CREATED BY ABDULAZIZ AND SAUD</p>
+  <p>Contact us</p>
+  <p>0539075201</p>
+</div>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>BODY FIT GYM</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="assets/style.css">
+  
+  <style>
+  .fakeimg {
+    height: 100px;
+    background-color: "#449D44";
+  }
+  </style>
+</head>
+<body>
+
+<div class="p-5 bg-primary text-white text-center">
+  <h1>WELCOME TO BODY FIT GYM</h1>
+  <p>MAKE YOUR LIFE HEALTHER AND FITNESS, REGISTER NOW</p> 
+</div>
